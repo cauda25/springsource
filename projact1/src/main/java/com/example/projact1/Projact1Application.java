a638824ec35ff89bc4c574ab0a14289fc4e24ad3
ju <roronoa246@gmail.com>
2024-10-24 13:19:37 +0900

    projact1 생성

---
+++ projact1/src/main/java/com/example/projact1/Projact1Application.java
@@ -0,0 +1,13 @@
+package com.example.projact1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projact1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projact1Application.class, args);
+	}
+
+}
